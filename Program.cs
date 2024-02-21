af016728b948b6345964bfe30eacaf5f1bdd1057
olie-r <160541542+olie-r@users.noreply.github.com>
2024-02-21 19:38:57 +0000

    do something

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please enter first number");
+
+string stringOne = Console.ReadLine();
+
+Console.WriteLine("Please enter second number");
+
+string stringTwo = Console.ReadLine();
+
+int numberOne = Convert.ToInt32(stringOne);
+int numberTwo = Convert.ToInt32(stringTwo);
+
+int sum = numberOne + numberTwo;
+
+Console.WriteLine(sum);
